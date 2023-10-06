da34fd00844c1e8726411a7dc4ce0727a4c73d25
Amaury Hamon <contact@amauryhamon.com>
2023-10-06 14:08:48 +0200

    title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Teaching Portal",
+    pageTitle: "Code 101",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
